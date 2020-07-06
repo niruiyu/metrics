8c3cdc247935d1ebefadeb2564abab2bb5f4495b
Ray Ni <ray.ni@intel.com>
2020-07-06 10:58:18 +0800

    use ArgumentParser in CountApiRef.py

--- CountApiRef.py
+++ CountApiRef.py
@@ -3,11 +3,10 @@ import sys
 import glob
 import re
 import threading
+import time
+import argparse
 
 FileType = ['.c']
-
-WORK_SPACE = r"E:\Work\edk2\\"
-
 
 APIRe = {}
 API = {}
@@ -18,7 +17,7 @@ Lock = threading.Lock()
   Count how many calls to each API from this module.
 '''
 def CountApiCall (module):
-  for c in glob.glob(f"{WORK_SPACE}{module}\\**", recursive=True):
+  for c in glob.glob(f"{module}\\**", recursive=True):
     if os.path.isfile (c) and os.path.splitext (c)[1].lower() in ['.c']:
       with open (c, 'r', encoding='ISO-8859-1') as f:
         for line in f:
@@ -33,20 +32,17 @@ def CountApiCall (module):
               Lock.release ()
 
 
-def Main():
+def Main(workspace, library, searchresult, module):
   # 1. collect all APIs from a lib header file
   # 2. get all module INF paths from VS code search result
   EfiApi = False
-  (LibH, SearchResult) = (sys.argv[1], sys.argv[2])
-
-
   # 1. collect all APIs from a lib header file
   #EFIAPI
   reEfiApi = re.compile (r'^\s*EFIAPI\s*$')
 
   #GetLocalApicBaseAddress (
   reApi = re.compile (r'^\s*([A-Za-z_][\w]*)\s*\(\s*$')
-  with open (LibH, 'r') as f:
+  with open (library, 'r') as f:
     for line in f:
       line = line.split("//")[0]
       if reEfiApi.match (line) is not None:
@@ -68,13 +64,18 @@ def Main():
   # 2. get all modules from VS code search result
 
   # IntelFsp2WrapperPkg\Library\SecFspWrapperPlatformSecLibSample\SecFspWrapperPlatformSecLibSample.inf:
-  reInfPath = re.compile (r'^([\w\\]+\w[.]inf):')
-  with open (SearchResult, 'r') as f:
+  reInfPath = re.compile (r'^(?:([\w]+)\s+â\x80¢\s+)?([\w\\]+\w[.]inf):$')
+  with open (searchresult, 'r', encoding='ISO-8859-1') as f:
     for line in f:
+      #line = line.rstrip ("\r\n")
       m = reInfPath.match (line)
       if m is not None:
         # Module -> { API -> # }
-        Module[os.path.dirname (m.group (1))] = []
+        if m.group(1) is not None:
+          Module[os.path.join (workspace, m.group (1), os.path.dirname (m.group (2)))] = []
+        else:
+          Module[workspace, os.path.dirname (m.group (2))] = []
+        print (m.group(1), m.group(2))
 
   # 3. for each module
   #      for each API
@@ -97,12 +98,26 @@ def Main():
 
     ApiCallCount = ApiCallCount + len(API[api])
 
+
   # API Usage Index
   print ("API Usage Index = {0:.3f}".format (ApiCallCount / (len(API) * len (Module))))
 
+  for api in API:
+    if module is not None:
+      for m in API[api]:
+        if m.find (module) != -1:
+          print (f"{api} is called by {m}: {API[api][m]}")
   return 0
 
-import time
 start = time.time ()
-Main()
+
+parser = argparse.ArgumentParser ()
+parser.add_argument ("-l", "--library", help="Path to the library header file.", type=str, required=True)
+parser.add_argument ("-s", "--searchresult", help="Search result from VS Code.", type=str, required=True)
+parser.add_argument ("-w", "--workspace", help="path to work space", type=str, required=True)
+parser.add_argument ("-m", "--module", help="Show APIs called from the specific module.", type=str)
+args = parser.parse_args ()
+
+
+Main(args.workspace, args.library, args.searchresult, args.module)
 print ("Time: {0}".format (time.time () - start))
